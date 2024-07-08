3c42bedd2363e1b6acd0206e743e4af520a31844
Dhinesh-Mech <dhinesh.bdsec@gmail.com>
2024-07-08 21:01:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+function add(){
+    let a=document.getElementById("num1").value;
+    let b=document.getElementById("num2").value;
+    let addresult=parseInt(a)+parseInt(b);
+    document.getElementById("result").innerHTML=addresult;
+}
+function sub(){
+    let a=document.getElementById("num1").value;
+    let b=document.getElementById("num2").value;
+    let addresult=parseInt(a)-parseInt(b);
+    document.getElementById("result").innerHTML=addresult;
+}
+
+function multi(){
+    let a=document.getElementById("num1").value;
+    let b=document.getElementById("num2").value;
+    let addresult=parseInt(a)*parseInt(b);
+    document.getElementById("result").innerHTML=addresult;
+}
+function divi(){
+    let a=document.getElementById("num1").value;
+    let b=document.getElementById("num2").value;
+    let addresult=parseInt(a)/parseInt(b);
+    document.getElementById("result").innerHTML=addresult;
+}
+function reset(){
+    let a=document.getElementById("num1").value;
+    let b=document.getElementById("num2").value;
+    let addresult=parseInt(a)+parseInt(b);
+    document.getElementById("result").innerHTML=addresult;
+}
